b3d657345dd846a23ac2ffb0711990ae815e32c5
st20220271 <kelly030813@kaist.ac.kr>
2024-05-01 15:19:21 +0900

    assdd

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState('');
+
+  const onChangeText = (inputText) => {
+    setText(inputText);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Signup</Text>
+      <TextInput style = {styles.input} onChangeText = {onChangeText} value = {text} placeholder = "Name"></TextInput>
+      <TextInput style = {styles.input} onChangeText = {onChangeText} value = {text} placeholder = "Name"></TextInput>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +25,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    borderColor: "#000000",
+    borderWidth: 1,
+    width: 200,
+    height: 40,
+    marginTop: 20,
+  },
+  burtton: {
+    backgroundColor: "EEEEEE",
   },
 });
